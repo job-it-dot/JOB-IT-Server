99d1b528962d579af09b59bbaa88f5102bd32b4f
HongBum(KOSTA) <jhbsp@naver.com>
2020-06-09 12:38:27 +0900

    db접속경로 수정, swaggerconfig수정, swaggerTestController추가

--- src/main/java/kosta/mvc/config/SwaggerConfiguration.java
+++ src/main/java/kosta/mvc/config/SwaggerConfiguration.java
@@ -14,19 +14,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
+
     @Bean
-    public Docket swaggerApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo.kosta.fin.api.controller"))
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("kosta.mvc.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200,401,403,404 메시지를 표시 하지 않음
-    }
-    
-    
-    private ApiInfo swaggerInfo() {
-        return new ApiInfoBuilder().title("Spring API Documentation")
-                .description("앱 개발시 사용되는 서버 API에 대한 연동 문서입니다")
-                .license("happydaddy").licenseUrl("http://daddyprogrammer.org").version("1").build();
+                .build();
+
     }
 }
