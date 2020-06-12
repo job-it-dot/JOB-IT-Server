f33084b97630e073048e7400f388af71304099a4
JinHyuck(KOSTA) <knd5191@naver.com>
2020-06-12 11:25:14 +0900

    ..

--- src/main/java/kosta/mvc/service/RecruitService.java
+++ src/main/java/kosta/mvc/service/RecruitService.java
@@ -15,31 +15,31 @@ public interface RecruitService {
 		//채용정보 상세조회
 		List<Recruit> selectRecruitById(int recruitId);
 		
-		//관심 기업 등록
+		//관심 기업 등록 result 성공 - 1, 실패 - 0
 		int registerInterestCompany(Follow follow);
 		
-		//관심 기업 해지
+		//관심 기업 해지 result 성공 - 1, 실패 - 0
 		int cancleInterestCompany(int followId);
 		
-		//관심기업 목록 조회
+		//관심기업 목록 조회 
 		List<Follow> selectAllInterestCompany();
 				
-		//관심기업 삭제
+		//관심기업 삭제 result 성공 - 1, 실패 - 0
 		int deleteInterestCompany(int followId);
 		
-		//채용공고 스크랩(등록)
+		//채용공고 스크랩(등록) result 성공 - 1, 실패 - 0
 		int scrapRecruit(Scrap scrap);
 		
-		//채용공고 스크랩 취소하기
+		//채용공고 스크랩 취소하기 result 성공 - 1, 실패 - 0
 		int cancleScrapRecruit(int scrapId);
 		
 		//스크랩 채용공고 목록 조회
 		List<Scrap> selectAllScrap();
 		
-		//스크랩 채용공고 삭제
+		//스크랩 채용공고 삭제 result 성공 - 1, 실패 - 0
 		int deleteScrapRecruit(int scrapId);
 		
-		//알림 등록.
+		//알림 등록. result 성공 - 1, 실패 - 0
 		int insertAlram(Alram alram);
 		
 		
