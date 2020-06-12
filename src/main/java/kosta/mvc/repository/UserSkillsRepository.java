506e36b797cb37593d9e39177ddf694c86a56936
HongBum(KOSTA) <jhbsp@naver.com>
2020-06-12 14:29:43 +0900

    userSkillsRepository update testRepository delete

--- src/main/java/kosta/mvc/repository/UserSkillsRepository.java
+++ src/main/java/kosta/mvc/repository/UserSkillsRepository.java
@@ -2,6 +2,8 @@ package kosta.mvc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserSkillsRepository extends JpaRepository<UserSkillsRepository, Long> {
+import kosta.mvc.domain.UserSkill;
+
+public interface UserSkillsRepository extends JpaRepository<UserSkill, Long> {
 
 }
