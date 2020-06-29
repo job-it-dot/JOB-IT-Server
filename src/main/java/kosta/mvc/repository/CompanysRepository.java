3b81b6ac938aa56145d5bbe79e9f994fbe52f24d
HongBum(home) <jhbsp@naver.com>
2020-06-29 22:45:01 +0900

    CompanysRepository Query update

--- src/main/java/kosta/mvc/repository/CompanysRepository.java
+++ src/main/java/kosta/mvc/repository/CompanysRepository.java
@@ -9,7 +9,7 @@ public interface CompanysRepository extends JpaRepository<Companys, Long> {
 
 	public Companys findByCompanyId(Long companyId);
 
-	@Query("SELECT c.company_id FROM companys c WHERE c.member_id=?1")
+	@Query("SELECT c.companyId FROM Companys c WHERE c.member.memberId=?1")
 	public Long findByMemberId(Long memberId);
 
 //	@Query("SELECT * FROM members m WHERE m.membe_id=?1")
