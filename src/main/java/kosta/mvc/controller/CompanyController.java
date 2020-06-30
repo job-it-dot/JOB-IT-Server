5febda47532e2c6d9a25a470ec2bb8928cb12790
KyungHwa(KOSTA) <khkim940@gmail.com>
2020-06-30 14:39:19 +0900

    company requestMapping 수정 -kh

--- src/main/java/kosta/mvc/controller/CompanyController.java
+++ src/main/java/kosta/mvc/controller/CompanyController.java
@@ -3,14 +3,14 @@ package kosta.mvc.controller;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,7 +38,7 @@ public class CompanyController {
 	private PasswordEncoder passwordEncoder;
 	
 	@ApiOperation(value = "기업 정보 조회", notes = "return : 기업회원 정보")
-	@RequestMapping("/info")
+	@PostMapping("/info")
 	public Companys info() throws IOException, NotFoundException {
 		Members member = (Members)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Long companyId = companyService.getCompanyId(member.getMemberId());
@@ -46,7 +46,7 @@ public class CompanyController {
 	}
 	
 	@ApiOperation(value = "비밀번호 확인", notes = "return : 0-비밀번호 불일치 / 1-일치")
-	@RequestMapping("/checkPwd")
+	@PostMapping("/checkPwd")
 	public int checkPwd(@ApiParam("입력받은 비밀번호")String password) throws IOException, NotFoundException{
 		int result = 0;
 
@@ -58,7 +58,7 @@ public class CompanyController {
 	}
 	
 	@ApiOperation(value = "기업정보(회원정보) 수정", notes = "return : 0-수정실패 / 1-성공")
-	@RequestMapping("/updateInfo")
+	@PostMapping("/updateInfo")
 	public int updateInfo(@ApiParam("수정할 기업 정보")Companys company) throws IOException, NotFoundException {
 		int result = companyService.updateCompany(company);
 		
@@ -77,7 +77,7 @@ public class CompanyController {
 	}
 	
 	@ApiOperation(value = "로그인한 기업회원이 올린 채용공고 목록 조회", notes = "return : 공고 list")
-	@RequestMapping("/recruitList")
+	@PostMapping("/recruitList")
 	public List<Recruit> recruitList() throws IOException, NotFoundException {
 		Members member = (Members)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Long companyId = companyService.getCompanyId(member.getMemberId());
@@ -85,31 +85,31 @@ public class CompanyController {
 	}
 	
 	@ApiOperation(value = "채용공고 상세보기", notes = "return : 채용공고내용")
-	@RequestMapping("/recruit/{recruitId}")
+	@GetMapping("/recruit/{recruitId}")
 	public Recruit recruitInfo(@ApiParam("링크로 넘어오는 채용공고 아이디")@PathVariable Long recruitId) throws IOException, NotFoundException {
 		return companyService.selectRecruitById(recruitId);
 	}
 	
 	@ApiOperation(value = "채용공고 등록", notes = "return : 0-등록실패 / 1-등록성공")
-	@RequestMapping("/uploadRecruit")
+	@PostMapping("/uploadRecruit")
 	public int uploadRecruit(@ApiParam("등록할 채용공고 정보")Recruit recruit) throws IOException {
 		return companyService.insertRecruit(recruit);
 	}
 	
 	@ApiOperation(value = "채용공고 수정", notes = "return : 0-수정실패 / 1-성공")
-	@RequestMapping("/updateRecruit")
+	@PostMapping("/updateRecruit")
 	public int updateRecruit(@ApiParam("수정할 채용공고 정보")Recruit recruit) throws IOException, NotFoundException {
 		return companyService.updateRecruit(recruit);
 	}
 	
 	@ApiOperation(value = "채용공고 내리기", notes = "return : 0-내리기 실패 / 1-성공")
-	@RequestMapping("/unpostRecruit/{recruitId}")
+	@GetMapping("/unpostRecruit/{recruitId}")
 	public int unpostRecruit(@ApiParam("내릴 채용공고의 아이디")@PathVariable Long recruitId) throws IOException, NotFoundException {
 		return companyService.unpostRecruit(recruitId);
 	}
 	
 	@ApiOperation(value = "예상 채용일정 목록 조회", notes = "return : 예상 채용일정 list")
-	@RequestMapping("/recruitPlanList")
+	@PostMapping("/recruitPlanList")
 	public List<RecruitPlan> recruitPlanList() throws IOException, NotFoundException {
 		Members member = (Members)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Long companyId = companyService.getCompanyId(member.getMemberId());
@@ -117,25 +117,25 @@ public class CompanyController {
 	}
 	
 	@ApiOperation(value = "예상 채용일정 상세보기", notes = "return : 예상 채용일정 내용")
-	@RequestMapping("/recruitPlan/{recruitPlanId}")
+	@GetMapping("/recruitPlan/{recruitPlanId}")
 	public RecruitPlan recruitPlanInfo(@ApiParam("링크로 넘어오는 예상 채용일정 아이디")@PathVariable Long recruitPlanId) throws IOException, NotFoundException {
 		return companyService.selectRecruitPlanById(recruitPlanId);
 	}
 	
 	@ApiOperation(value = "예상 채용일정 등록", notes = "return : 0-등록실패 / 1-등록성공")
-	@RequestMapping("/uploadRecruitPlan")
+	@PostMapping("/uploadRecruitPlan")
 	public int uploadRecruitPlan(@ApiParam("등록할 예상 채용일정 정보")RecruitPlan recruitPlan) throws IOException {
 		return companyService.insertRecruitPlan(recruitPlan);
 	}
 	
 	@ApiOperation(value = "예상 채용일정 수정", notes = "return : 0-수정실패 / 1-성공")
-	@RequestMapping("/updateRecruitPlan")
+	@PostMapping("/updateRecruitPlan")
 	public int updateRecruitPlan(@ApiParam("수정할 예상 채용일정 정보")RecruitPlan recruitPlan) throws IOException, NotFoundException {
 		return companyService.updateRecruitPlan(recruitPlan);
 	}
 	
 	@ApiOperation(value = "예상 채용일정 삭제", notes = "return : 0-삭제실패 / 1-성공")
-	@RequestMapping("/deleteRecruitPlan/{recruitPlanId}")
+	@GetMapping("/deleteRecruitPlan/{recruitPlanId}")
 	public int deleteRecruitPlan( @ApiParam("삭제할 예상 채용일정 아이디")@PathVariable Long recruitPlanId) throws IOException, NotFoundException {
 		Members member = (Members)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Long companyId = companyService.getCompanyId(member.getMemberId());
@@ -143,31 +143,31 @@ public class CompanyController {
 	}
 	
 	@ApiOperation(value = "한 공고의 지원 리스트", notes = "return : 지원(Apply table)의 list")
-	@RequestMapping("/applyList/{recruitId}")
+	@GetMapping("/applyList/{recruitId}")
 	public List<Apply> applyList(@ApiParam("지원자를 확인하고 싶은 채용공고의 아이디")@PathVariable Long recruitId) throws IOException {
 		return companyService.selectApplyByRecruitId(recruitId);
 	}
 	
 	@ApiOperation(value = "한 공고의 지원 상세보기", notes = "return : 지원(Apply table)의 정보")
-	@RequestMapping("/apply/{applyId}")
+	@GetMapping("/apply/{applyId}")
 	public Apply applyInfo(@ApiParam("확인할 지원아이디")@PathVariable Long applyId) throws IOException, NotFoundException {
 		return companyService.selectApplyById(applyId);
 	}
 	
 	@ApiOperation(value = "웹 최상단 공고 노출 패키지 구매", notes = "return : 0-구매실패 / 1-성공")
-	@RequestMapping("/perchase")
+	@PostMapping("/perchase")
 	public int perchase(@ApiParam("구매 정보")Perchase perchase) throws IOException {
 		return companyService.insertPerchase(perchase);
 	}
 	
 	@ApiOperation(value = "패키지 환불신청", notes = "return : 0-신청실패 / 1-성공")
-	@RequestMapping("/refund/{perchaseId}")
+	@GetMapping("/refund/{perchaseId}")
 	public int refund(@ApiParam("환불신청할 구매내역의 아이디")@PathVariable Long perchaseId) throws IOException, NotFoundException, RuntimeException {
 		return companyService.updatePerchase(perchaseId);
 	}
 	
 	@ApiOperation(value = "오픈 이력서 목록 조회", notes = "return : 오픈 이력서 list")
-	@RequestMapping("/openResume")
+	@PostMapping("/openResume")
 	public List<Resume> openResume() throws IOException, NotFoundException, RuntimeException {
 		Members member = (Members)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Long companyId = companyService.getCompanyId(member.getMemberId());
@@ -175,7 +175,7 @@ public class CompanyController {
 	}
 	
 	@ApiOperation(value = "오픈 이력서 상세보기", notes = "return : 오픈 이력서 내용")
-	@RequestMapping("/openResume/{resumeId}")
+	@GetMapping("/openResume/{resumeId}")
 	public Resume openResumeInfo(@ApiParam("링크로 넘어오는 예상 채용일정 아이디")@PathVariable Long resumeId) throws IOException, NotFoundException {
 		Members member = (Members)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Long companyId = companyService.getCompanyId(member.getMemberId());
